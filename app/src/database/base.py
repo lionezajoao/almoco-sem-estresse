0fcf8de3f4510b2839719ed677b598b4b61a79f9
lionezajoao <joaopedrobzlioneza@hotmail.com>
2024-03-25 21:06:06 -0300

    Added connection controll between methods

--- app/src/database/base.py
+++ app/src/database/base.py
@@ -1,49 +1,69 @@
 import os
 import psycopg2
 
-
-
 class Database:
     def __init__(self):
-        self.conn = psycopg2.connect(
-            host=os.environ.get("PG_HOST"),
-            database=os.environ.get("PG_DATABASE"),
-            user=os.environ.get("PG_USER"),
-            password=os.environ.get("PG_PASS"),
-            port=os.environ.get("PG_PORT")
-        )
-        self.cur = self.conn.cursor()
+        pass
 
-    def create(self, query):
-        self.cur.execute(query)
-        self.commit()
-
-    def query(self, query, params=None):
-        self.cur.execute(query, params)
+    def connect(self):
         try:
-            return self.cur.fetchall()
+            self.conn = psycopg2.connect(
+                host=os.environ.get("PG_HOST"),
+                database=os.environ.get("PG_DATABASE"),
+                user=os.environ.get("PG_USER"),
+                password=os.environ.get("PG_PASS"),
+                port=os.environ.get("PG_PORT")
+            )
+            self.cur = self.conn.cursor()
         except Exception as e:
             print(e)
-            return None
-    
-    def insert(self, query, params=None):
-        self.cur.execute(query, params)
-        self.commit()
-
-    def update(self, query, params=None):
-        self.cur.execute(query, params)
-        self.commit()
-
-    def delete(self, query, params=None):
-        self.cur.execute(query, params)
-        self.commit()
-
-    def rollback(self):
-        self.conn.rollback()
-
-    def commit(self):
-        self.conn.commit()
 
     def close(self):
         self.cur.close()
         self.conn.close()
+
+    def create(self, query):
+        self.connect()
+        self.cur.execute(query)
+        self.commit()
+        self.close()
+
+    def query(self, query, params=None):
+        self.connect()
+        self.cur.execute(query, params)
+        try:
+            data = self.cur.fetchall()
+            self.close()
+            return data
+        except Exception as e:
+            print(e)
+            self.close()
+            return None
+    
+    def insert(self, query, params=None):
+        self.connect()
+        self.cur.execute(query, params)
+        self.commit()
+        self.close()
+
+    def update(self, query, params=None):
+        self.connect()
+        self.cur.execute(query, params)
+        self.commit()
+        self.close()
+
+    def delete(self, query, params=None):
+        self.connect()
+        self.cur.execute(query, params)
+        self.commit()
+        self.close()
+
+    def rollback(self):
+        self.connect()
+        self.conn.rollback()
+        self.close()
+
+    def commit(self):
+        self.connect()  
+        self.conn.commit()
+        self.close()
